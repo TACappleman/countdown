59bae85a2093779291bfb8eac579745d13eae6ef
Thomas Cappleman <tcappleman@microsoft.com>
2022-02-01 16:48:20 +0000

    Run rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::io::{stdout, stdin, Write};
+use std::io::{stdin, stdout, Write};
+use std::sync::mpsc::{self, TryRecvError};
 use std::thread::{self, JoinHandle};
 use std::time::Duration;
-use std::sync::mpsc::{self, TryRecvError};
 
 fn main() {
     println!("How many larges would you like? (0-4)");
@@ -9,7 +9,10 @@ fn main() {
     let mut larges = String::new();
     stdin().read_line(&mut larges).expect("Failed to read line");
 
-    let larges: u32 = larges.trim().parse().expect("Please input a number next time");
+    let larges: u32 = larges
+        .trim()
+        .parse()
+        .expect("Please input a number next time");
 
     let round = countdown::Round::new(larges);
     println!("Your round is: {}", round);
@@ -23,29 +26,29 @@ fn main() {
 }
 
 fn setup_threads() -> JoinHandle<()> {
-	let (tx, rx) = mpsc::channel();
+    let (tx, rx) = mpsc::channel();
 
     thread::spawn(move || {
-    	println!("Press enter to end round early");
-    	let mut input = String::new();
-	    stdin().read_line(&mut input).expect("Failed to read line");
-    	let _ = tx.send(());
+        println!("Press enter to end round early");
+        let mut input = String::new();
+        stdin().read_line(&mut input).expect("Failed to read line");
+        let _ = tx.send(());
     });
 
     thread::spawn(move || {
         for i in 0..30001 {
-        	if i % 1000 == 0 {
-	            print!("\r{} ", 30-i/1000);
-	            stdout().flush().unwrap();
-        	}
+            if i % 1000 == 0 {
+                print!("\r{} ", 30 - i / 1000);
+                stdout().flush().unwrap();
+            }
             thread::sleep(Duration::from_millis(1));
 
-	        match rx.try_recv() {
-	            Ok(_) | Err(TryRecvError::Disconnected) => {
-	                break;
-	            }
-	            Err(TryRecvError::Empty) => {}
-	        }
+            match rx.try_recv() {
+                Ok(_) | Err(TryRecvError::Disconnected) => {
+                    break;
+                }
+                Err(TryRecvError::Empty) => {}
+            }
         }
     })
 }
